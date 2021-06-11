c9a0eecbfa022bd63eec689432c08d822b4c91ff
thirtydays <boris.myasnikov@hotmail.com>
2021-06-11 13:45:50 +0700

    Mapbox Firebase Example

--- app/src/main/java/com/example/geomapfirebase/MainActivity.java
+++ app/src/main/java/com/example/geomapfirebase/MainActivity.java
@@ -2,20 +2,24 @@ package com.example.geomapfirebase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.location.LocationEngineResult;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Feature;
@@ -28,16 +32,19 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 
 import com.google.firebase.database.DatabaseReference;
+import com.mapbox.mapboxsdk.plugins.localization.LocalizationPlugin;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
@@ -51,13 +58,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public static final double r = 6372.8;
 
     private DatabaseReference dbReference;
-    private List<Coordinate> coordinates;
+    //массив для хранения всех координат
+    public List<Coordinate> coordinates;
+    //массив для хранения по текущей локации
+    public List<Coordinate> showCoords;
 
     private MapView mapView;
     private MapboxMap map;
     private PermissionsManager permissionsManager;
-    private String latitude;
-    private String longitude;
+    private LocationEngine locationEngine;
+    private long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L;
+    private long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
+    private MainActivityLocationCallback callback = new MainActivityLocationCallback(this);
 
     private static final String SOURCE_ID = "SOURCE_ID";
     private static final String ICON_ID = "ICON_ID";
@@ -72,11 +84,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
         coordinates = new ArrayList<Coordinate>();
+        showCoords = new ArrayList<Coordinate>();
         dbReference = FirebaseDatabase.getInstance().getReference("coordinate");
-    }
-
-    public void onClick(View v) {
-       onMapReady(map);
     }
 
     //формула хаверсина
@@ -90,22 +99,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return r * c;
     }
 
-    @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+    public List<Feature> getMarkers() {
         List<Feature> markers = new ArrayList<>();
         //загрузка данных по массиву координат
-        for(int i=0; i<coordinates.size(); i++) {
-            Feature loc = Feature.fromGeometry(Point.fromLngLat(Double.parseDouble(coordinates.get(i).getLng()),
-                    Double.parseDouble(coordinates.get(i).getLat())));
+        for(int i=0; i<showCoords.size(); i++) {
+            Feature loc = Feature.fromGeometry(Point.fromLngLat(Double.parseDouble(showCoords.get(i).getLng()),
+                    Double.parseDouble(showCoords.get(i).getLat())));
             loc.addStringProperty("NAME_PROPERTY_KEY"+i, "description"+i);
             markers.add(loc);
         }
+        return markers;
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
         MainActivity.this.map = mapboxMap;
         mapboxMap.setStyle(new Style.Builder().fromUri(Style.MAPBOX_STREETS)
                         .withImage(ICON_ID, BitmapFactory.decodeResource(
                                 MainActivity.this.getResources(), R.drawable.mapbox_marker_icon_default))
                         .withSource(new GeoJsonSource(SOURCE_ID,
-                                FeatureCollection.fromFeatures(markers)))
+                                FeatureCollection.fromFeatures(getMarkers())))
                         .withLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
                                 .withProperties(
                                         iconImage(ICON_ID),
@@ -119,41 +132,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
+                        //установка локализации
+                        LocalizationPlugin localizationPlugin = new LocalizationPlugin(mapView, mapboxMap, style);
+                        try {
+                            localizationPlugin.matchMapLanguageWithDeviceDefault();
+                        } catch (RuntimeException exception) {
+                            Log.d(TAG, exception.toString());
+                        }
                         enableLocationComponent(style);
                     }
                 });
     }
 
-    @SuppressLint("WrongConstant")
+    @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
             LocationComponent locationComponent = map.getLocationComponent();
-            locationComponent.activateLocationComponent(
-                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                    PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                    PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
+            LocationComponentActivationOptions locationComponentActivationOptions =
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                            .useDefaultLocationEngine(false)
+                            .build();
+            locationComponent.activateLocationComponent(locationComponentActivationOptions);
             locationComponent.setLocationComponentEnabled(true);
-            locationComponent.setCameraMode(CameraMode.TRACKING_GPS);
+            locationComponent.setCameraMode(CameraMode.TRACKING);
             locationComponent.setRenderMode(RenderMode.COMPASS);
-            Location lastLocation = locationComponent.getLastKnownLocation();
-            //получение текущего местоположения
-            latitude = Double.toString(lastLocation.getLatitude());
-            longitude = Double.toString(lastLocation.getLongitude());
+            initLocationEngine();
         } else {
             permissionsManager = new PermissionsManager(this);
             permissionsManager.requestLocationPermissions(this);
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this);
+        LocationEngineRequest request = new LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+                .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+        locationEngine.requestLocationUpdates(request, callback, getMainLooper());
+        locationEngine.getLastLocation(callback);
     }
 
     @Override
@@ -189,16 +206,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 coordinates.clear();
                 for(DataSnapshot postSnapshot : snapshot.getChildren()) {
                     Coordinate coordinate = postSnapshot.getValue(Coordinate.class);
-                    //если текущее местоположение определено
-                    if((!latitude.equals(""))&&(!longitude.equals(""))){
-                        //если расстояние между координатами меньше 10км
-                        if(haversine(Double.parseDouble(latitude),
-                                     Double.parseDouble(longitude),
-                                     Double.parseDouble(coordinate.getLat()),
-                                     Double.parseDouble(coordinate.getLng()))<10)
-                            //добавляем объект в массив
                             coordinates.add(coordinate);
-                    }
+                            showCoords.add(coordinate);
                 }
             }
             @Override
@@ -207,10 +216,53 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.onStart();
     }
 
+    private static class MainActivityLocationCallback
+            implements LocationEngineCallback<LocationEngineResult> {
+        private final WeakReference<MainActivity> activityWeakReference;
+        MainActivityLocationCallback(MainActivity activity) {
+            this.activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onSuccess(LocationEngineResult result) {
+            MainActivity activity = activityWeakReference.get();
+            if (activity != null) {
+                Location location = result.getLastLocation();
+                if (location == null) {
+                    return;
+                }
+                activity.showCoords.clear();
+                if (activity.map != null && result.getLastLocation() != null) {
+                    activity.map.getLocationComponent().forceLocationUpdate(result.getLastLocation());
+                    for(int i=0; i<activity.coordinates.size(); i++) {
+                        if(haversine(Double.parseDouble(String.valueOf(result.getLastLocation().getLatitude())),
+                                     Double.parseDouble(String.valueOf(result.getLastLocation().getLongitude())),
+                                     Double.parseDouble(activity.coordinates.get(i).getLat()),
+                                     Double.parseDouble(activity.coordinates.get(i).getLng()))<10) {
+                            activity.showCoords.add(activity.coordinates.get(i));
+                        }
+                    }
+                    activity.getMarkers();
+                }
+                activity.mapView.getMapAsync(activity);
+            }
+        }
+
+        @Override
+        public void onFailure(@NonNull Exception exception) {
+            MainActivity activity = activityWeakReference.get();
+            if (activity != null) {
+                Toast.makeText(activity, exception.getLocalizedMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         mapView.onResume();
+        mapView.getMapAsync(this);
     }
 
     @Override
@@ -240,6 +292,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (locationEngine != null) {
+            locationEngine.removeLocationUpdates(callback);
+        }
         mapView.onDestroy();
     }
 
